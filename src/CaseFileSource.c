90c67e3c8f72db575b3c1872189414b75bcab46a
David Deepwell <ddeepwel@uwaterloo.ca>
2017-10-31 23:42:17 -0400

    Make derivatives.x not overwrite spinscase.cpp
    The spinscase.cpp file is the case file showing what was run in
    a given simulation. If derivatives are calculated afterwards
    (using derivatives.x) then this file gets over-written and case
    specific parameters/instructions will be lost. Rather than be
    over-written, a different file (derivatives.cpp) will be created
    when derivatives.x is run.

--- src/CaseFileSource.c
+++ src/CaseFileSource.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 /* Write the source code of the case to spinscase.cpp */
 void WriteCaseFileSource(void)
 {
-   FILE* fid;
-   fid=fopen("spinscase.cpp","w");
-   fprintf(fid,"/* %s */\n%s",casefilename,casefilesource);
-   fclose(fid);
+    char* filename;
+    if ( strcmp(casefilename, "cases/derivatives/derivatives.cpp") ) {
+        filename = "derivatives.cpp";
+    } else {
+        filename = "spinscase.cpp";
+    }
+    FILE* fid;
+    fid = fopen(filename,"w");
+    fprintf(fid,"/* %s */\n%s",casefilename,casefilesource);
+    fclose(fid);
 }
